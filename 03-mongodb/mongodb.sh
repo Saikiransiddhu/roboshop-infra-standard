26a4c01fba3bb0a9ef41f0dce4cfebadab522c35
saikiran <saikiransiddhu26@gmail.com>
2023-09-28 21:29:45 +0530

    roboshop new

--- 03-mongodb/mongodb.sh
+++ 03-mongodb/mongodb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /tmp
-git clone https://github.com/Saikiransiddhu/roboshop-infra-standard.git
+git clone https://github.com/Saikiransiddhu/roboshop-shell.git
 cd roboshop-shell
 sh mongodb.sh
